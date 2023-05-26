432e312a4231b4facf63c826cb969c9b8b782790
Keven Presseau <EMAIL>
2023-05-26 14:19:04 -0400

    feat: :sparkles: implement nested listing

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Template:
 ```
 {
     "Single": "{SingleLabel}",
-    "Multiple": "{ListOfInfos:[Info1]/[Info2]}"
+    "Multiple": "{ListOfInfos~[Info1]/[Info2]}",
+    "Nested" : "{NestedInfos~[firstSingle]/[secondSingle]([innerList~[name]:[value]])}"
 }
 ```
 Results
@@ -20,6 +21,27 @@ Results
         {
             "Info1": "Tim",
             "Info2": "Smith"
+        }
+    ],
+    "NestedInfos": [
+        {
+            "firstSingle": "ABBA",
+            "secondSingle": "collection",
+            "innerList": [
+                {
+                    "name": "Animal",
+                    "value": "Wolf"
+                },
+                {
+                    "name": "Animal2",
+                    "value": "Dog"
+                }
+            ]
+        },
+        {
+            "firstSingle": "ACDC",
+            "secondSingle": "collection",
+            "innerList": []
         }
     ]
 }
@@ -36,6 +58,7 @@ Output:
 ```
 {
     "Single": "SingleField",
-    "Multiple": "Bob/Gratton, Tim/Smith" 
+    "Multiple": "Bob/Gratton, Tim/Smith",
+    "Nested" : "ABBA/collection(Animal:Wolf, Animal2:Dog), ACDC/collection"
 }
 ```
--- src/main/java/test/json/FieldMapper.java
+++ src/main/java/test/json/FieldMapper.java
@@ -7,8 +7,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,6 +17,7 @@ import com.fasterxml.jackson.databind.node.TextNode;
 public class FieldMapper {
 
     private static final String INSIDE_CURLY_BRACES = "^\\{(.*)\\}$";
+    private static final String DELIMITER = "~";
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public static String map(JsonNode template, JsonNode results) {
@@ -29,6 +28,7 @@ public class FieldMapper {
             throw new IllegalArgumentException();
         }   
     }
+    
     private static void recursiveSwap(JsonNode template, JsonNode results) {
         Iterator<Entry<String, JsonNode>> fields = template.fields();
         while (fields.hasNext()) {
@@ -38,54 +38,88 @@ public class FieldMapper {
 
             if (fieldValue.isValueNode()) {
                 if (fieldValue.isTextual() && fieldValue.asText().contains("{")) {
-                    valueSwap(template, results, fieldName, fieldValue);
+                    valueSwap(template, results, fieldName, fieldValue.asText().replaceAll(INSIDE_CURLY_BRACES, "$1"));
                 }
             } else {
                 recursiveSwap(fieldValue, results);
             }
         }
     }
-    private static void valueSwap(JsonNode template, JsonNode results, String fieldName, JsonNode fieldValue){
-        String operations = fieldValue.asText().replaceAll(INSIDE_CURLY_BRACES, "$1");
-        if (operations.contains(":")){
-            multiValueSwap(template, results, fieldName, operations);
+
+    private static void valueSwap(JsonNode template, JsonNode results, String fieldName, String op){
+        if (op.contains(DELIMITER)){
+            String[] ops = splitString(op);
+            String outer = ops[0];
+            String inner = ops[1];
+            JsonNode outerNode = results.path(outer);
+            String resultString = multiValueSwap(outerNode, outer, inner);
+            ((ObjectNode) template).replace(fieldName, new TextNode(resultString));
         } else {
-            ((ObjectNode) template).replace(fieldName, results.findValue(operations));
+            ((ObjectNode) template).replace(fieldName, results.findValue(op));
         }
     }
-    private static void multiValueSwap(JsonNode template, JsonNode results, String fieldName, String operations){
-        String[] ops = operations.split(":");
-        String outer = ops[0];
-        String inner = ops[1];
-        JsonNode outerNode = results.path(outer);
+
+    private static String multiValueSwap(JsonNode outerNode, String outer, String inner){
+        String finalString = "";
+
         if(inner.contains("[")){
             List<String> labels = getLabelsInBraces(inner);
             Map<String, List<JsonNode>> data = new HashMap<>();
             for(String label: labels){
-                data.put(label, outerNode.findValues(label));
+                if (label.contains(DELIMITER)){
+                    String[] nestedLabels = splitString(label);
+                    String nestedOuter = nestedLabels[0];
+                    String nestedInner = nestedLabels[1];
+                    List<JsonNode> nesteds = outerNode.findValues(nestedOuter);
+                    List<JsonNode> nestedValues = new ArrayList<>();
+                    for (JsonNode nested : nesteds){
+                        String nestV = multiValueSwap(nested, nestedOuter, nestedInner);
+                        nestedValues.add(new TextNode(nestV));
+                    }
+                    data.put(label, nestedValues);
+                } else {
+                    data.put(label, outerNode.findValues(label));
+                }
+                
             }
-            String finalString = "";
             final int SIZE = data.get(labels.get(0)).size();
             for(int i = 0; i < SIZE; i++){
                 String resultTemplate = inner;
-
                 for(String label: labels){
                     resultTemplate = resultTemplate.replace("[" + label + "]", data.get(label).get(i).asText());
                 }
                 String lastChar = (i == SIZE - 1) ? "" : ", ";
                 finalString = finalString + resultTemplate + lastChar;
             }
-            ((ObjectNode) template).replace(fieldName, new TextNode(finalString));
+        } else {
+            finalString += outerNode.path(inner).asText();
         }
+        return finalString.replace("()", "");
     }
+
     public static List<String> getLabelsInBraces(String input) {
-        List<String> labels = new ArrayList<>();
-        Pattern pattern = Pattern.compile("\\[([^\\[\\]]+)\\]");
-        Matcher matcher = pattern.matcher(input);
-        while (matcher.find()) {
-            String word = matcher.group(1);
-            labels.add(word);
+        List<String> words = new ArrayList<>();
+        int bracketCount = 0;
+        String label = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == ']') bracketCount--;
+            if (bracketCount >= 1) label += c;
+            if (c == '[') bracketCount++;
+            if (bracketCount == 0 && !label.equals("")) {
+                words.add(label);
+                label = "";
+            }
         }
-        return labels;
+        return words;
+    }
+    
+    public static String[] splitString(String input) {
+        int splitIndex = input.indexOf(DELIMITER);
+        String[] splitArray = new String[2];
+        splitArray[0] = input.substring(0, splitIndex);
+        splitArray[1] = input.substring(splitIndex + 1);
+        return splitArray;
     }
 }
